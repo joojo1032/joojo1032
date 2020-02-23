a858efaa6b0bd4af0c8a94aa6876d0cf8718ff51
jeff98gh <jdegrafthinson@gmail.com>
2020-02-23 05:21:28 +0000

    reworked

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export class Home extends React.Component{
     render(){
         return(
             <div>
-                
+                <h>Let's call it a day</h>
             </div>
         )
     }
