06971b7be44ee326cb5d7cd898c541640e19d06a
jeff98gh <jdegrafthinson@gmail.com>
2020-02-25 23:12:32 +0000

    reworked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Header.css';
+import logo from '../images/logo.jpg';
+import {Avatar} from 'antd'
+
+
 
 export class Head extends React.Component{
     render(){
         return(
             <div className="header-container">
                     <div className="brand-container">
-                        <h2>Logo</h2>
+                        <div className="brand-logo"><Avatar size={60} shape={"square"} src={logo} alt="logo"/></div>
                     </div>
-                    <nav className="nav-container">
-                        <ul className="nav-content">
-                            <li><Link className="link" to="/">Home</Link></li>
-                            <li><Link className="link" to="/Communities">Communities</Link></li>
-                            <li><Link className="link" to="/Inspector">Inspector</Link></li>
-                            <li><Link className="link" to="/Admin">Admin</Link></li>
-                        </ul>
-                    </nav>
+                    <ul className="nav-content">
+                        <Link className="link" to="/">Home</Link>
+                        <Link className="link" to="/Communities">Communities</Link>
+                        <Link className="link" to="/Inspector">Inspector</Link>
+                        <Link className="link" to="/Admin">Admin</Link>
+                    </ul>
             </div>
         )
     }
