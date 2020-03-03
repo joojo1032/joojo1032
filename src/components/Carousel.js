c758ad459c72c872fb71a0cf80ece4a244919587
joojo1032 <you@example.com>
2020-03-03 13:22:07 +0000

    reworked

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -9,14 +9,13 @@ export class Slider extends React.Component{
             <Carousel autoplay effect="fade">
                 <div className="image-box">
                     <div className="content-box">
-                        <h1 className="content-header">Sustainable</h1>
-                        <h2 className="content-header2">Development Goals</h2>
-                        <Button type="primary" size="large" className="btn">Learn More</Button>
+                        <h1 className="slider-content">Sustainable Development Goals</h1>
+                        <Button type="primary" size="large" >Learn More</Button>
                     </div>
                 </div>
                 <div className="image-box2">
                     <div className="content-box">
-                        <h2 className="slider-content">Clean Water And Sanitation</h2>
+                        <h2 className="slider-content">Clean Water And Sanitation-Goal 6</h2>
                         <Button type="primary" size="large">Learn More</Button>
                     </div>
                 </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -7,7 +7,11 @@ export class Content extends React.Component{
         return(
             <div>
                <Card>
-                    <p>Type the SDG info here</p>
+                    <p>Sustainable Development Goal 6</p>
+                    <p>Sustainable Development Goal 6 is one of 17 Sustainable Development Goals established by the United Nations General Assembly in 2015. It calls for clean water and sanitation for all people. The official wording is: "Ensure availability and sustainable management of water and sanitation for all.</p> 
+                    <p>Clean, accessible water for all is an essential part of the world we want to live in and there is sufficient fresh water on the planet to achieve this. However, due to bad economics or poor infrastructure, millions of people including children die every year from diseases associated with inadequate water supply, sanitation and hygiene.</p>
+                    <p>Water scarcity, poor water quality and inadequate sanitation negatively impact food security, livelihood choices and educational opportunities for poor families across the world. At the current time, more than 2 billion people are living with the risk of reduced access to freshwater resources and by 2050, at least one in four people is likely to live in a country affected by chronic or recurring shortages of fresh water. Drought in specific afflicts some of the world’s poorest countries, worsening hunger and malnutrition. Fortunately, there has been great progress made in the past decade regarding drinking sources and sanitation, whereby over 90% of the world’s population now has access to improved sources of drinking water.</p>
+                    <p>To improve sanitation and access to drinking water, there needs to be increased investment in management of freshwater ecosystems and sanitation facilities on a local level in several developing countries within Sub-Saharan Africa, Central Asia, Southern Asia, Eastern Asia and South-Eastern Asia.</p>
                </Card>
             </div>
         )
