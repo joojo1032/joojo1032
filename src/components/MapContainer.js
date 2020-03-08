2e79677a9c5c8251613a98f5b85d095c631c69b6
jeff98gh <jdegrafthinson@gmail.com>
2020-03-08 15:26:15 +0000

    reworked

--- src/components/MapContainer.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import { MapContainer } from './Map';
-
-
-
-export default GoogleApiWrapper({
-    apiKey: 'AIzaSyBN-zGf8Lv0WVw4sdBHm6jpmSFUx-_Rof4'
-  })(MapContainer);
