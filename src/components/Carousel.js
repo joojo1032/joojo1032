ae293017e222d03e0fd5263cd2652af7b8049ec7
joojo1032 <you@example.com>
2020-03-16 20:18:11 -0400

    reworked

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,19 +10,19 @@ export class Slider extends React.Component{
                 <div className="image-box">
                     <div className="content-box">
                         <h1 className="slider-content">Sustainable Development Goals</h1>
-                        <Button type="primary" size="large" >Learn More</Button>
+                        <Button type="primary" size="large" href="https://www.undp.org/content/undp/en/home/sustainable-development-goals/background/">Learn More</Button>
                     </div>
                 </div>
                 <div className="image-box2">
                     <div className="content-box">
                         <h2 className="slider-content">Clean Water And Sanitation-Goal 6</h2>
-                        <Button type="primary" size="large">Learn More</Button>
+                        <Button type="primary" size="large" href="https://www.undp.org/content/undp/en/home/sustainable-development-goals/goal-6-clean-water-and-sanitation.html">Learn More</Button>
                     </div>
                 </div>
                 <div className="image-box3">
                     <div className="content-box">
                         <h2 className="slider-content">17 Sustainable Development Goals</h2>
-                        <Button type="primary" size="large">Learn More</Button>
+                        <Button type="primary" size="large" href="https://www.undp.org/content/undp/en/home/sustainable-development-goals.html">Learn More</Button> 
                     </div>
                 </div>
             </Carousel>
